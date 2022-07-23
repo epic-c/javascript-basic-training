445792fca01b31b1c9160199cfc760c15eb13eff
admin <epic.cc@icloud.com>
2022-07-23 22:35:48 +0800

    update floder schema

---
+++ example/table/searchTable.js
@@ -0,0 +1,17 @@
+let table = document.querySelector("table")
+let tr = table.getElementsByTagName("tr")
+const searchKey = "End";
+
+let result = []
+for (let i = 0; i < tr.length; i++) {
+     if(tr[i].getElementsByTagName("td")[4].innerText === searchKey){
+        result.push(tr[i])
+    }
+}
+
+for (let i = 0; i < result.length; i++) {
+    result[i]
+        .getElementsByTagName("td")[0]
+        .querySelector("input")
+        .click()
+}
