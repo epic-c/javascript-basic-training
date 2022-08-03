ea9be09d3718e66918d6c9b74ef05a9a49a0cb89
admin <epic.cc@icloud.com>
2022-08-03 23:12:42 +0800

    format

--- questions/js-questions-20220731.js
+++ questions/js-questions-20220731.js
@@ -6,15 +6,14 @@
 // 重度肥胖：高於30
 // 極度肥胖：高於40
 
-
 function q1(height, weight) {
     // let result = "";
     // let BMI =weight/(height/100)/(height/100)
     // if(BMI<18.5){
     //     result="過輕"
-    // }    
+    // }
     // //這行也可以寫成return "過輕"(以下類推) 而且也因為都return掉，就不用寫return result
-    // // 而且也因為return就中止，所以就也不用寫else if 
+    // // 而且也因為return就中止，所以就也不用寫else if
     // // 參數是讓人傳值用的，所以盡量不要修改他的值；變數則是自行宣告使用
 
     // else if(BMI>18.5&&BMI<22.9){
@@ -34,35 +33,34 @@ function q1(height, weight) {
     // }
     // // ...
     // return result;
-    let BMI =weight/(height/100)/(height/100)
+    let BMI = weight / (height / 100) / (height / 100);
 
-//     weight = 10
-//     if(BMI<18.5){
-//        return "過輕"
-//     }    
-//     if(BMI>18.5&&BMI<22.9){
-//         return "正常"
-//     }
-//     if(BMI>23&&BMI<24.9){
-//         return "偏胖"
-//     }
-//     if(BMI>25&&BMI<29.9){
-//         return "肥胖"
-//     }
-//     if(BMI>30&&BMI<39){
-//         return "重度肥胖"
-//     }
-//     if(BMI>40){
-//         return "極度肥胖"
-//     }
+    //     weight = 10
+    //     if(BMI<18.5){
+    //        return "過輕"
+    //     }
+    //     if(BMI>18.5&&BMI<22.9){
+    //         return "正常"
+    //     }
+    //     if(BMI>23&&BMI<24.9){
+    //         return "偏胖"
+    //     }
+    //     if(BMI>25&&BMI<29.9){
+    //         return "肥胖"
+    //     }
+    //     if(BMI>30&&BMI<39){
+    //         return "重度肥胖"
+    //     }
+    //     if(BMI>40){
+    //         return "極度肥胖"
+    //     }
 }
 
 //X<15 && X>10 且的概念
 //X<15 || X>10 或的概念
 // X!=15 代表X不等於15
 // !X<15 代表後面指令的相反，也就是X要大於等於15
-console.log(q1(180,60))
-
+console.log(q1(180, 60));
 
 // 手機選號:根據使用者輸入的手機號來確認使用者實際支付的價格
 // 如果尾數為8，需支付辦卡費50元
@@ -87,27 +85,27 @@ function q3(number) {
         // 24 = 24 * 5(i)
         // 120 = 120 * 6(i)
         // ...
-        result = result * i
+        result = result * i;
     }
-    
-    return result
+
+    return result;
 }
 
-console.log(q3(3))
+console.log(q3(3));
 //進去看參數；出去看回傳
 
 // 100以內除了尾數為3,5,7的所有數
 function q4() {
     let result = [];
-    for(let i=1;i<=100;i++){
-        if(i%10!=3&&i%10!=5&&i%10!=7){
+    for (let i = 1; i <= 100; i++) {
+        if (i % 10 != 3 && i % 10 != 5 && i % 10 != 7) {
             result.push(i);
         }
     }
     // ...
     return result;
 }
-console.log(q4())
+console.log(q4());
 
 // 泡沫排序法
 function q5() {
